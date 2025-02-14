d55b17451513a5b016a0d70705ce1cc146513131
HAL <68320771+HALQME@users.noreply.github.com>
2025-02-14 12:31:07 +0900

    ニュースページを追加し、APIからのデータ取得を実装。不要な依存関係を削除し、型定義を更新。

--- src/pages/api/utils/news.ts
+++ src/pages/api/utils/news.ts
@@ -1,31 +1,37 @@
 import { getCollection, type CollectionEntry } from "astro:content";
-
-const newsletters = await getCollection("newsletters");
+import { AstroError } from "astro/errors";
+import type { JSONData } from "@/src/type";
+import { z } from "astro/zod";
+import { sheetSchemaToZodSchema } from "astro-sheet-loader";
 
 // URL Parameter
 // - `start`: string || null : 掲載開始日
 // - `end`: string || null : 掲載終了日
 // - `long`: boolean || null : False OR Nullでは内容を省略
-export function GET(request: Request) {
+export async function GET(request: Request) {
     const host = new URL(request.url).hostname;
     const { searchParams } = new URL(request.url);
     const startParam = searchParams.get("start");
     const endParam = searchParams.get("end");
     const isLong = searchParams.get("long") === "true";
 
+    const url =
+        "https://docs.google.com/spreadsheets/d/1DgzrtZRnkOAhHSw2R85lRutATlXB-K9p-W9HiHfzF2Q/gviz/tq?tqx=out:json";
+
     const startDate = startParam ? new Date(startParam) : null;
     const endDate = endParam ? new Date(endParam) : null;
     const today = new Date();
     today.setHours(0, 0, 0, 0);
 
-    const filteredNewsletters = newsletters
-        .filter((entry: CollectionEntry<"newsletters">) => {
-            // ホスト名との一致をチェック
-            if (host !== "localhost" && entry.data.ターゲット !== host)
-                return false;
+    const newsletters = await sheetToJson(url).then(parseSheet);
 
-            const entryStartDate = new Date(entry.data.掲載開始日);
-            const entryEndDate = new Date(entry.data.掲載終了日);
+    const filteredNewsletters = newsletters
+        .filter((entry) => {
+            // ホスト名との一致をチェック
+            if (host !== "localhost" && entry.target !== host) return false;
+
+            const entryStartDate = new Date(entry.startDate);
+            const entryEndDate = new Date(entry.endDate);
 
             // 掲載開始日のチェック
             if (startDate) {
@@ -45,7 +51,7 @@ export function GET(request: Request) {
         })
         .map((entry) => {
             if (!isLong) {
-                entry.data.内容 = "";
+                entry.body = "";
             }
             return entry;
         });
@@ -62,5 +68,97 @@ export function GET(request: Request) {
             "content-type": "application/json",
             "Access-Control-Allow-Origin": acao,
         },
+    });
+}
+
+// ref: https://github.com/AlphaJack/astro-sheet-loader/commit/98f626e5b925525b54d8ac264751259958833a26
+export async function sheetToJson(url: string): Promise<JSONData> {
+    const response = await fetchJSON(url);
+    const text = await response.text();
+    return parseJSON(text, url);
+}
+async function fetchJSON(url: string): Promise<Response> {
+    return fetch(url).catch((error: Error) => {
+        throw new AstroError(`Error fetching ${url}: ${error}`);
+    });
+}
+async function parseJSON(text: string, url?: string): Promise<JSONData> {
+    //const document = url?.split("/")[5];
+    if (text.startsWith("<!DOCTYPE html>")) {
+        throw new AstroError(
+            `Error fetching JSON data for '${url}', check the ID and share settings of the document.`
+        );
+    }
+    let jsonObject: JSONData;
+    const jsonString = text.slice(47, -2);
+    try {
+        jsonObject = JSON.parse(jsonString);
+    } catch (error) {
+        throw new AstroError(`Error parsing JSON data for '${url}': ${error}`);
+    }
+    if (jsonObject.status === "error") {
+        throw new AstroError(
+            `Error in JSON data for '${url}': ${jsonObject.errors?.[0]?.detailed_message ?? "Unknown error"}`
+        );
+    }
+    return jsonObject;
+}
+
+const newsSchema = z.object({
+    A: z.string(), //更新日
+    B: z.string().email(), // 作成者
+    C: z.string(), // 掲載開始日
+    D: z.string(), // 掲載終了日
+    E: z.enum(["omu-aikido.com", "app.omu-aikido.com"]), // ターゲット
+    F: z.enum(["High", "Medium", "Low", "Caution"]), // 重要度
+    G: z.string(), // タイトル
+    H: z.string().optional(), // 概要
+    I: z.string(), // 本文
+});
+
+export const newsCollection = z.object({
+    updatedAt: z.date(),
+    createdBy: z.string().email(),
+    startDate: z.date(),
+    endDate: z.date(),
+    target: z.enum(["omu-aikido.com", "app.omu-aikido.com"]),
+    priority: z.enum(["High", "Medium", "Low", "Caution"]),
+    title: z.string(),
+    summary: z.string().optional(),
+    body: z.string(),
+});
+
+async function parseSheet(
+    data: JSONData
+): Promise<z.infer<typeof newsCollection>[]> {
+    const rows = data.table.rows;
+    // ヘッダー行をスキップ
+    const dataRows = rows.slice(1);
+
+    const entries = dataRows.map((row) => {
+        const entry: any = {};
+        row.c.forEach((cell, index) => {
+            const column = data.table.cols[index].id;
+            if (cell) {
+                entry[column] = cell.v;
+            }
+        });
+        return entry;
+    });
+
+    return entries.map((entry) => {
+        const validatedEntry = newsSchema.parse(entry);
+
+        return newsCollection.parse({
+            updatedAt: new Date(validatedEntry.A),
+            createdBy: validatedEntry.B,
+            startDate: new Date(validatedEntry.C),
+            endDate: new Date(validatedEntry.D),
+            target: validatedEntry.E,
+            priority: validatedEntry.F,
+            title: validatedEntry.G,
+            summary: validatedEntry.H,
+            body: validatedEntry.I,
+        });
     });
 }
--- src/type.d.ts
+++ src/type.d.ts
@@ -8,3 +8,39 @@ export type RoleType =
     | "member";
 export type Year = "b1" | "b2" | "b3" | "b4" | "m1" | "m2" | "d1" | "d2";
 export type Grade = 0 | 5 | 4 | 3 | 2 | 1 | -1 | -2;
+
+export interface JSONData {
+    version: string;
+    reqId: string;
+    status: string;
+    errors?: ErrorData[];
+    sig: string;
+    table: TableData;
+}
+
+export interface ErrorData {
+    reason: string;
+    message: string;
+    detailed_message: string;
+}
+
+export interface TableData {
+    cols: Column[];
+    rows: Row[];
+}
+
+export interface Row {
+    c: (Cell | null)[];
+}
+
+export interface Column {
+    id: string;
+    label: string;
+    type: string;
+    pattern?: string;
+}
+
+export interface Cell {
+    f?: string;
+    v: string | number | null;
+}
