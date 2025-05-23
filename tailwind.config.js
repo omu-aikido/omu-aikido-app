97265443fb2d1e447bb5a741b6d00d2dcd0357e5
HAL <68320771+HALQME@users.noreply.github.com>
2025-05-23 13:42:09 +0900

    Tailwind CSSの設定を追加

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}",
+        "./public/**/*.html",
+    ],
+    plugins: [
+        require("@catppuccin/tailwindcss")({
+            defaultFlavour: "latte",
+        }),
+        require("@tailwindcss/typography"),
+    ],
+    darkMode: "class",
+};
