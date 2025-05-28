0fa0d2785019f1c7e202ca5c80eb34409f959eeb
HAL <68320771+HALQME@users.noreply.github.com>
2025-05-29 00:18:47 +0900

    Tailwind CSS + Actionsで安全かつ美麗なUXを導入

---
+++ src/actions/user.ts
@@ -0,0 +1,11 @@
+import { defineAction } from "astro:actions";
+
+export const user = {
+    // add: defineAction({
+    //     handler: (input, context) => {
+    //         return { success: true };
+    //     },
+    // }),
+    // update: defineAction({}),
+    // delete: defineAction({}),
+};
